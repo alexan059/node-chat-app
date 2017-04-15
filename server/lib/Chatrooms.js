394cc3b8276f864ab486b5604266d75d551dd8e6
Alexander <accounts.alexan059@gmail.com>
2017-04-15 15:03:57 +0200

    Added feature hidden rooms and refactored the classes

--- server/lib/Chatrooms.js
+++ server/lib/Chatrooms.js
@@ -1,19 +1,33 @@
+const {EventEmitter} = require('events');
 const _ = require('lodash');
 
 let rooms = null;
 
-class Chatrooms {
+class Chatrooms extends EventEmitter {
 
     constructor() {
+        super();
         this.rooms = [];
     }
 
     join(socketId, userName, roomParams) {
-        let room = {
-            name: roomParams.name,
-            isHidden: (typeof roomParams.isHidden !== 'undefined' && parseInt(roomParams.isHidden) === 1),
-            users: []
-        };
+        // Find room if exists
+        let room = _.find(this.rooms, (room) => room.name === roomParams.name);
+
+        // Create room if it doesn't exist
+        if (!room) {
+            room = {
+                name: roomParams.name,
+                isHidden: (typeof roomParams.isHidden !== 'undefined' && parseInt(roomParams.isHidden) === 1),
+                users: []
+            };
+
+            // Add room
+            this.rooms.push(room);
+        }
+
+        // Leave user from old room before assigning to new one
+        this.leave(socketId);
 
         let user = {
             id: socketId,
@@ -21,22 +35,72 @@ class Chatrooms {
             room: room
         };
 
+        // Add user to the room
         room.users.push(user);
-        this.rooms.push(room);
+
+        // Emit the update room list event
+        this.emit('updateRoomList', this.getRoomList());
 
         return user;
     }
 
     leave(socketId) {
+        // Get the user
+        let user = this.getUser(socketId);
+
+        if (user) {
+            // Remove the user from the room
+            user.room.users = _.filter(user.room.users, (user) => user.id !== socketId);
+
+            // Remove room if empty
+            if (user.room.users.length === 0) {
+                this.rooms = _.filter(this.rooms, (room) => room.name !== user.room.name);
+            }
+
+            // Emit the update room list event
+            this.emit('updateRoomList', this.getRoomList());
+
+            // Get the user back
+            return user;
+        }
+    }
+
+    getUser(socketId) {
+        // Find the room containing the user
         let room = _.find(this.rooms, (room) => {
             return (_.findIndex(room.users, (user) => user.id === socketId) !== -1);
         });
 
-        room.users = _.filter(room.users, (user) => user.id !== socketId);
+        // Return the user if room exists
+        if (room) {
+            return _.find(room.users, (user) => user.id === socketId);
+        }
+    }
+
+    getUserList(roomName) {
+        // Get the room
+        let room = _.find(this.rooms, (room) => room.name === roomName);
+
+        // Return empty array if room doesn't exist
+        if (!room) {
+            return []
+        }
+
+        // Return all user names of given room
+        return _.map(room.users, (user) => user.name);
+    }
+
+    getRoomList() {
+        // Find all visible rooms
+        let rooms = _.filter(this.rooms, (room) => !room.isHidden);
+
+        // Return the room names
+        return _.map(rooms, (room) => room.name);
     }
 
 }
 
+// Create a global instance
 module.exports.getInstance = () => {
     if (!rooms) {
         rooms = new Chatrooms();
@@ -45,6 +109,7 @@ module.exports.getInstance = () => {
     return rooms;
 };
 
+// Reset instance for testing
 module.exports.resetInstance = () => {
     rooms = new Chatrooms();
 };
